c2c3fc5a3aa75cd64071dede16cf340993cb5ff9
sunwentao <sunwentao@imcoming.cn>
2016-10-17 17:58:55 +0800

    "增加美女拼图游戏"

---
+++ WangYi/app/src/main/java/com/yanzhe/swt/wangyi/Agame/ImagePiece.java
@@ -0,0 +1,12 @@
+package com.yanzhe.swt.wangyi.Agame;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by User on 2016/10/17.
+ */
+
+public class ImagePiece {
+    public int index = 0;
+    public Bitmap bitmap = null;
+}
